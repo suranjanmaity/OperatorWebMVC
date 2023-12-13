08510b6a138a5b3f6d716f722478d0aca84914cd
suranjanmaity <suranjanmaity4@gmail.com>
2023-12-13 19:24:19 +0530

    Add project files.

---
+++ OperatorWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OperatorWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
